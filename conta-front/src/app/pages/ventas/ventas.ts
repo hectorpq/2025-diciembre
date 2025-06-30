c2e8afb8911490e9e787f10473efb544300bcb46
hectorpq <pacompiahectorrobert@gmail.com>
2025-06-30 09:03:33 -0500

    8085

--- conta-front/src/app/pages/ventas/ventas.ts
+++ conta-front/src/app/pages/ventas/ventas.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-ventas',
-  imports: [],
+  standalone: true,
   templateUrl: './ventas.html',
-  styleUrl: './ventas.scss'
+  styleUrls: ['./ventas.scss'],
+  imports: [CommonModule, FormsModule, HttpClientModule]
 })
-export class Ventas {
+export class Ventas implements OnInit {
+  ventas: any[] = [];
+  nuevaVenta: any = {
+    clienteNombre: '',
+    clienteEmail: '',
+    clienteTelefono: '',
+    fecha: new Date().toISOString(),
+    detalles: []
+  };
+  detalle = { productoId: null, descripcion: '', cantidad: 1, precioUnitario: 0.0, descuento: 0.0 };
 
+  mostrarFormulario = false;
+  mensaje = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.cargarVentas();
+  }
+
+  cargarVentas() {
+    this.http.get<any[]>('http://localhost:8085/api/ventas').subscribe(data => {
+      this.ventas = data;
+    });
+  }
+
+  agregarDetalle() {
+    if (this.detalle.descripcion && this.detalle.cantidad > 0 && this.detalle.precioUnitario > 0) {
+      this.nuevaVenta.detalles.push({ ...this.detalle });
+      this.detalle = { productoId: null, descripcion: '', cantidad: 1, precioUnitario: 0.0, descuento: 0.0 };
+    }
+  }
+
+  quitarDetalle(i: number) {
+    this.nuevaVenta.detalles.splice(i, 1);
+  }
+
+  guardarVenta() {
+    this.http.post('http://localhost:8085/api/ventas', this.nuevaVenta).subscribe(() => {
+      this.cargarVentas();
+      this.mostrarFormulario = false;
+      this.mensaje = 'Venta registrada con éxito';
+      setTimeout(() => this.mensaje = '', 3000);
+      this.nuevaVenta = {
+        clienteNombre: '',
+        clienteEmail: '',
+        clienteTelefono: '',
+        fecha: new Date().toISOString(),
+        detalles: []
+      };
+    });
+  }
+
+  cancelar() {
+    this.mostrarFormulario = false;
+    this.nuevaVenta = {
+      clienteNombre: '',
+      clienteEmail: '',
+      clienteTelefono: '',
+      fecha: new Date().toISOString(),
+      detalles: []
+    };
+  }
 }
